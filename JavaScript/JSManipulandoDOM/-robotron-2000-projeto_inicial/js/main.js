b01e1f27eaa39ece8b201289ccd79b6eee704405
Lucas Burch <lucasburch@hotmail.com>
2022-07-18 17:10:30 -0300

    Finalizando o curso
    Uma certa dificuldade em me acostumar com o uso de data-attributes, mas é outro curso que continua com o desenvolvimento do site, que tá só acrescentando estatísticas e não descontando. Não consegui fazer a tarefa final de mudança de imagens; o curso avançou rápido demais nas tarefas de interação. Acredito que o Challenge vai ter menos peso de conteúdo nesse sentido.

--- JavaScript/JSManipulandoDOM/-robotron-2000-projeto_inicial/js/main.js
+++ JavaScript/JSManipulandoDOM/-robotron-2000-projeto_inicial/js/main.js
@@ -1,27 +1,75 @@
-const braco = document.querySelector('#braco');
-
 // Arrays das classes que usamos:
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica");
 
-controle.forEach( (elemento) => {
+/*Exercício final de mudança de cor do robô:*/ 
+
+const robotron = document.querySelector("#robotron");
+
+
+const pecas = {
+  "bracos": {
+    "forca": 29,
+    "poder": 35,
+    "energia": -21,
+    "velocidade": -5
+  },
+
+  "blindagem": {
+    "forca": 41,
+    "poder": 20,
+    "energia": 0,
+    "velocidade": -20
+  },
+  "nucleos": {
+    "forca": 0,
+    "poder": 7,
+    "energia": 48,
+    "velocidade": -24
+  },
+  "pernas": {
+    "forca": 27,
+    "poder": 21,
+    "energia": -32,
+    "velocidade": 42
+  },
+  "foguetes": {
+    "forca": 0,
+    "poder": 28,
+    "energia": 0,
+    "velocidade": -2
+  }
+};
+
+controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
-    manipulaDados(evento.target.textContent, evento.target.parentNode);
+    manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+    atualizaEstatisticas(evento.target.dataset.peca);
   }) // (conteúdo do botão (+ ou -), elemento "pai" do input (div classe controle))
 });
 
 function manipulaDados(operacao, controle) {
 
-  const peca = controle.querySelector(".controle-contador");
-  // a div de classe controle é pai do contador
-  // logo, é o endereço de onde estamos clicando
+  // mudamos de classes iguais às html/css para data-attributes, para evitar falhas de código em caso de modificação do textContent, que deu lugar na chamada do manipulaDados para o ".dataset.controle"
+  const peca = controle.querySelector("[data-contador]");
 
-    if (operacao === "-") {
-      if (parseInt(peca.value) > 0) {
-        peca.value = parseInt(peca.value) - 1;
-      }
-    } else {
-      peca.value = parseInt(peca.value) + 1;
+  /* a div de classe controle é pai do contador
+  logo, é o endereço de onde estamos clicando:
+  Braços, Blindagem, Núcleos, Pernas ou Foguetes*/
+
+  if (operacao === "-") {
+    if (parseInt(peca.value) > 0) {
+      peca.value = parseInt(peca.value) - 1;
     }
+  } else {
+    peca.value = parseInt(peca.value) + 1;
+  }
+}
+
+function atualizaEstatisticas(peca) {
+  estatisticas.forEach((elemento) => {
+    elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+  });
 }
 
 
