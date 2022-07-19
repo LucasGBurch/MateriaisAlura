c4ba6d2ee9e39f46e8b6f4ce7918f05a21ae3bf1
Lucas Burch <lucasburch@hotmail.com>
2022-07-19 19:11:33 -0300

    Apenas ajustes de indentação

--- JavaScript/JSArmazenandoDadosNav/mochila-de-viagem-main/js/main.js
+++ JavaScript/JSArmazenandoDadosNav/mochila-de-viagem-main/js/main.js
@@ -4,7 +4,7 @@ const itens = JSON.parse(localStorage.getItem("itens")) || []
 
 itens.forEach( (elemento) => {
     criaElemento(elemento)
-} )
+})
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
@@ -40,23 +40,23 @@ form.addEventListener("submit", (evento) => {
 })
 
 function criaElemento(item) {
-    const novoItem = document.createElement("li")
-    novoItem.classList.add("item")
+    const novoItem = document.createElement("li");
+    novoItem.classList.add("item");
 
-    const numeroItem = document.createElement("strong")
-    numeroItem.innerHTML = item.quantidade
-    numeroItem.dataset.id = item.id
-    novoItem.appendChild(numeroItem)
-    
-    novoItem.innerHTML += item.nome
+    const numeroItem = document.createElement("strong");
+    numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
 
-    novoItem.appendChild(botaoDeleta(item.id))
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += item.nome;
 
-    lista.appendChild(novoItem)
+    novoItem.appendChild(botaoDeleta(item.id));
+
+    lista.appendChild(novoItem);
 }
 
 function atualizaElemento(item) {
-    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
 }
 
 function botaoDeleta(id) {
