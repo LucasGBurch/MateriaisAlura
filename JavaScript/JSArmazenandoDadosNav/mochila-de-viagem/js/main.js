bb7ecaeb74bc482439d19ab407ffecbff66b7830
Lucas Burch <lucasburch@hotmail.com>
2022-07-22 11:16:04 -0300

    Aulas 1 e 2 Refeitas
    Baixei um arquivo JS incompleto a partir da segunda aula e acompanhei a criação dos métodos e objeto de nome e quantidade do formulário de armazenamento com mais calma. Agora deu pra entender melhor a manipulação deles, a criação de novos itens de lista e, principalmente, a criação de um Array desses objetos convertidos em string pelo stringify para ser aceito dentro do localStorage da página. Agora sim deu pra começar a entender como funciona o armazenamento de dados no navegador e dar continuidade ao curso para aplicar seus conceitos no Challenge FrontEnd.

--- JavaScript/JSArmazenandoDadosNav/mochila-de-viagem/js/main.js
+++ JavaScript/JSArmazenandoDadosNav/mochila-de-viagem/js/main.js
@@ -1,21 +1,45 @@
-const form = document.getElementById("novoItem")
-const lista = document.getElementById("lista")
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = [];
 
 form.addEventListener("submit", (evento) => {
-    evento.preventDefault()
+    evento.preventDefault(); // Evita recarregar a página, comportamento padrão do submit de um formulário;
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
-})
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
+
+    // evento do formulário.alvo.elementos['nome do input'].valor
+    criaElemento(nome.value, quantidade.value);
+
+    nome.value = "";
+    quantidade.value = "";
+});
 
 function criaElemento(nome, quantidade) {
-    const novoItem = document.createElement('li')
-    novoItem.classList.add("item")
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item");
 
-    const numeroItem = document.createElement('strong')
-    numeroItem.innerHTML = quantidade
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
 
-    novoItem.appendChild(numeroItem)
-    novoItem.innerHTML += nome
+    novoItem.appendChild(numeroItem); // Nº com <strong> add ao 'li' via appendChild, para não criar um objeto;
+    novoItem.innerHTML += nome; // Nome acrescentado; como é só uma string, pode ser adicionada via innerHTML
 
-    lista.appendChild(novoItem)
-}
+    lista.appendChild(novoItem);
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    };
+
+    itens.push(itemAtual);
+
+
+    // 1) Isto só adiciona um nome/quantidade na memória local (Application, Storage, localStorage), sobrescrevendo o anterior, apesar dos elementos de lista estarem sendo criados na tela antes de um refresh;
+
+    localStorage.setItem("item", JSON.stringify(itens));
+
+    // 2) Em vez de adicionar nome e quantidade, adicionamos um objeto com os dois, mas, para ser lido não só como objeto do tipo Object, precisamos converter em string via stringify, do JSON. O localStorage só guarda strings;
+
+    // 3) Agora que o objeto fica legível no Storage, precisamos criar um array de itens lá em cima para criar um Array de Objetos. Por isso, em vez do item atual, passamos a adicionar pelo setItem acima o Array atualizado, ou seja, após o .push(itemAtual).
+};
