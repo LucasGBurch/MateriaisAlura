680d939698a45247d5bdb43d2c8a1a091bfab51a
Lucas Burch <lucasburch@hotmail.com>
2022-07-24 08:56:19 -0300

    Finalização do  curso de armazenar dados
    Aulas sobre atualização e remoção de itens tanto dos elementos quanto do Array de localStorage que usamos para lê-los e apresentar nos elementos. São tantas anotações que deixei documentadas no código numericamente.

--- JavaScript/JSArmazenandoDadosNav/mochila-de-viagem/js/main.js
+++ JavaScript/JSArmazenandoDadosNav/mochila-de-viagem/js/main.js
@@ -14,27 +14,40 @@ form.addEventListener("submit", (evento) => {
     const quantidade = evento.target.elements['quantidade']
     // evento do formulário.alvo.elementos['nome do input'].valor
 
-    // Para atualizar itens com nome existente (diferente de vazio-undefined):
+// 6) Para atualizar itens com nome existente (diferente de vazio-undefined, ele encontra o objeto caso exista com o mesmo nome):
     const existe = itens.find(elemento => elemento.nome === nome.value);
+// "O método find() retorna o valor do primeiro elemento do array que satisfizer a função de teste provida. Caso contrario, undefined é retornado."
 
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value,
     };
 
+// "No Javascript, se o valor de uma variável ou constante for 0, -0, null, false, NaN, undefined ou uma string vazia(""), o objeto terá um valor inicial de false.":
     if (existe) { // adicionamos id ao objeto se existir:
-        itemAtual.id = existe.id;
+        itemAtual.id = existe.id; // id do existente
 
         atualizaElemento(itemAtual);
-    } else { // senão, criamos o elemento primeiro e o id será o tamanho do Array!
+// 7) Para mudar no localStorage, além de atualizar o elemento, precisamos sobrescrever no Array (modificado depois do método deletar (8), para garantir que foi atualizado o elemento correto):
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
+// 7) Item cadastrado substituindo sua versão anterior que já existia;
 
-        itemAtual.id = itens.length; // começa em 0, quando não há itens ainda
+    } else {
+// senão, criamos o elemento primeiro e o id será o tamanho do Array!
+
+// Começa em 0, quando não há itens ainda, uma posição para o primeiro elemento (alteramos com ternário por causa da lógica de deletar):
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;  
+// Se existe o id da última posição .length - 1, somamos 1 para criar o id seguinte na numeração. Senão, ele será 0, o primeiro;
+
+
         criaElemento(itemAtual);
         itens.push(itemAtual);
     }
 
     localStorage.setItem("itens", JSON.stringify(itens));
+// 7) O conteúdo sobrescrito, itens[existe.id], que em (8) mudou para itens[findIndex(elemento => elemento.id === existe.id)], nos elementos que já existem também são passados aqui depois do if/else.
 
+// Resetar valor dos campos:
     nome.value = "";
     quantidade.value = "";
 });
@@ -50,16 +63,52 @@ function criaElemento(item) {
     novoItem.appendChild(numeroItem); // Nº com <strong> add ao 'li' via appendChild, para não criar um objeto;
     novoItem.innerHTML += item.nome; // Nome acrescentado; como é só uma string, pode ser adicionada via innerHTML
 
+// 8) agora, todo elemento criado terá seu botão de deletar:
+    novoItem.appendChild(botaoDeleta(item.id));
+
     lista.appendChild(novoItem);
 
 };
 
 function atualizaElemento(item) {
-    // 6) Testa no console.log() e tenta adicionar item repetido (detalhe encardido esse das aspas simples para envolver o número)
-    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade; // mudando seu innerHTML com o itemAtual.quantidade, ele é atualizado!! Falta agora o do localStorage, senão volta ao valor anterior!!
+// 6) Testa no console.log() e tenta adicionar item repetido (detalhe encardido esse das aspas simples para envolver o número)
 
-    // 7)
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade; 
+// 6) Mudando seu innerHTML com o itemAtual.quantidade, ele é atualizado!! Falta agora o do localStorage, senão volta ao valor anterior!!
+
 };
+
+// 8) Hora de criar o botão para deletar um item:
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerHTML = "X";
+
+// precisa ser função implícita, não arrow, para sabermos qual nó (this) está sendo clicado no HTML
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id);
+    }); // parentNode do botão é o li;
+
+    return elementoBotao;
+};
+
+// 8) E a função correspondente ao click do botão:
+
+function deletaElemento(tag, id) {
+    tag.remove();
+// fosse só this, removia o botão, por isso na chamada da tag a gente chama this.parentNode;
+
+// 8) Agora precisamos remover um item do Array, por isso precisamos passar o id à função E LÁ NO BOTÃO DA CRIAÇÃO DO ELEMENTO...
+
+// Aqui, achamos o elemento cujo id é igual ao do botão e removemos:
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+// splice(índice a remover, qtd de elementos a remover, elemento a adicionar (opcional))
+
+//... e reescrever o Array no localStorage:
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+};
+
 
     // 1) localStorage.setItem inicialmente só adicionava um nome/quantidade na memória local (Application, Storage, localStorage), sobrescrevendo o anterior, apesar dos elementos de lista estarem sendo criados na tela antes de um refresh;
 
